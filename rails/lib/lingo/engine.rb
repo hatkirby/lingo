787745956f13bb63c596ed80a53b386708b56508
Star Rauchenberger <fefferburbia@gmail.com>
2022-12-10 12:27:28 -0500

    Assets need to be precompiled

--- rails/lib/lingo/engine.rb
+++ rails/lib/lingo/engine.rb
@@ -3,5 +3,9 @@ require 'haml'
 module Lingo
   class Engine < ::Rails::Engine
     isolate_namespace Lingo
+
+    initializer "pokeviewer.assets" do |app|
+      app.config.assets.precompile += %w(lingo/header.png)
+    end
   end
 end
